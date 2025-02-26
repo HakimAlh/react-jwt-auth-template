c2a33e6c4b6e6d30a60169dfc1cc8d61a68843e1
Hakim <hakimalhaddad2@gmail.com>
2025-02-26 09:21:40 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+// src/App.jsx
+
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+
+const App = () => {
+  const [user, setUser] = useState(null)
+
+  return (
+    <>
+      <NavBar user={user} />
+      <h1>Hello world!</h1>
+    </>
+  )
+}
+
+export default App
